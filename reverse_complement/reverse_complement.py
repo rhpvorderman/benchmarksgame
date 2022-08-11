b8537182a4e934b10f86300d371d475fdd5ecbb3
Ruben Vorderman <RubenVorderman@xs4all.nl>
2022-08-11 08:14:14 +0200

    Parse fasta properly

--- reverse_complement/reverse_complement.py
+++ reverse_complement/reverse_complement.py
@@ -3,17 +3,28 @@ from typing import BinaryIO, Iterator, Tuple
 
 
 def parse_fasta(inp: BinaryIO) -> Iterator[Tuple[bytes, bytes]]:
-    pass
+    name = next(inp)
+    lines = []
+    for line in inp:
+        if line.startswith(b">"):
+            yield name, b"".join(lines)
+            name = line
+            lines = []
+            continue
+        lines.append(line)
+    yield name, b"".join(lines)
 
 
 def reverse_complement(inp: BinaryIO, outp: BinaryIO):
+    letters = b"ACGTUMRYKVHDB"
+    complmn = b"TGCAAKYRMBDHV"
     translate_table = bytes.maketrans(
-        b"ACGTUMRYKVHDB",
-        b"TGCAAKYRMBDHV",
+        letters + letters.lower(),
+        complmn + complmn,
     )
-    line_length = 80
+    line_length = 60
     for name, sequence in parse_fasta(inp):
-        translated = sequence.translate(translate_table)
+        translated = sequence.translate(translate_table, b'\n')
         reversed = translated[::-1]
         fasta_lines = [reversed[i:i+line_length]
                        for i in range(0, len(reversed), line_length)]
